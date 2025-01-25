557c4905bce60f9bbf88bcb1750fe8bbe9853c79
Arka@p <99315231+Arkaaap@users.noreply.github.com>
2025-01-25 20:24:19 +0530

    Create Bitblt1.cpp

---
+++ Bitblt1.cpp
@@ -0,0 +1,12 @@
+#include <windows.h>
+
+int main(){
+		while(1){
+		HDC hdc = GetDC(0);
+    	int x = GetSystemMetrics(0);
+    	int y = GetSystemMetrics(1);
+    	StretchBlt(hdc, -10, -10, x + 20, y + 20, hdc, 0, 0, x, y, NOTSRCCOPY);
+    	StretchBlt(hdc, 10, 10, x - 20, y - 20, hdc, 0, 0, x, y, NOTSRCERASE); //A little Distress Effect .
+    	ReleaseDC(0, hdc);
+	}
+}
